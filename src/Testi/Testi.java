8f5669a8db1a022a0662fca59f4a449b9df7852e
Nevil Sandaradura <nevil.sandaradura@gmail.com>
2020-03-02 10:59:38 +0200

    testi laitettu

---
+++ src/Testi/Testi.java
@@ -0,0 +1,11 @@
+package Testi;
+
+import Varausjärjestelmä.*;
+
+public class Testi {
+
+    public static void main(String[] args){
+        Asiakas a = new Asiakas("Asiakas", 19);
+        
+    }
+}
