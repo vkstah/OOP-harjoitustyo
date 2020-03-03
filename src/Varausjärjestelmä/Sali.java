d2271cba9c92c45b6da2d32e6d5c03d29baab50d
Teemu Salonen <tpsalo@utu.fi>
2020-03-03 15:25:35 +0200

    Saliin lisätty vapaidenPaikkojenLkm-metodin toteutus

--- src/Varausjärjestelmä/Sali.java
+++ src/Varausjärjestelmä/Sali.java
@@ -24,6 +24,25 @@ public abstract class Sali {
     }
 
     public int annaVapaidenPaikkojenLkm() {
+        int summa = 0;
+        if(paikat.length == 5) {
+            for (int i = 0; i<5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (paikat[i][j] == 1) {
+                        summa++;
+                    }
+                }
+            }
+        }else{
+            for (int i = 0; i < 7; i++) {
+                for (int j = 0; j < 7; j++) {
+                    if (paikat[i][j] == 1) {
+                        summa++;
+                    }
+                }
+            }
+        }
+        vapaidenPaikkojenLkm = summa;
         return vapaidenPaikkojenLkm;
     }
 
